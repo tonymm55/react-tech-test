c99078975035629fe7b406c65f6f2d881564cbf1
TonyMM55 <tonymmoran@gmail.com>
2023-04-03 21:39:01 +0100

    initial set up of files and folders

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/app.css";
+
+const App = () => {
+  return (
+    <div className="app">
+        <p>Hello World!
+        </p>
+    </div>
+  );
+}
+
+export default App;
