b7d3429e54685edebfdb11db518e5b0fe8ea356b
TonyMM55 <tonymmoran@gmail.com>
2023-04-04 18:28:57 +0100

    created 2 component files, App & Search+test+css files

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "../styles/app.css";
+import Search from "./Search";
 
 const App = () => {
   return (
     <div className="app">
-        <p>Hello World!
-        </p>
+      <img
+        className="nasa-logo"
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="nasaLogo" 
+      />
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "../styles/search.css";
+
+const Search = () => {
+    return (
+        <>
+          <input className="search-input" type="text" />
+        </>
+    );
+};
+
+export default Search;
+
