7076a020af0c633e7c05237a537a24b3862de7dd
TonyMM55 <tonymmoran@gmail.com>
2023-04-06 21:15:25 +0100

    modified getImages.js to render the array, return images is not working

--- src/requests/getImages.js
+++ src/requests/getImages.js
@@ -4,13 +4,16 @@ const getImages = (query) => {
   if (!query) {
     return Promise.resolve([]);
   } else {
-    return axios
+    return axios 
       .get(`https://images-api.nasa.gov/search?q=${query}`)
       .then((response) => {
-        console.log(response)
+          const imageResults = response.data.collection.items;
+          const parsedImages = imageResults.filter(result => result.data[0].media_type === "image");
+          const images = parsedImages.map(image => image.links[0].href);
+          console.log (images);
       })
       .catch((err) => {
-        console.log(err);
+        console.error(err);
       });
   }
 };
